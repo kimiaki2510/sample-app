f36df42f39ac65c5688d0e7ad7f935ae9a0ae7d5
kimiaki2510 <153046sk@gmail.com>
2020-08-02 13:12:51 +0900

    vuetify-downgrade

---
+++ src/components/SideNav.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-layout wrap style="height: 200px;">
+  <v-navigation-drawer v-model="$store.state.drawer" absolute temporary>
+    <v-list>
+      <v-list-item>
+        <v-list-item-avatar>
+          <img v-if="photoURL" :src="photoURL">
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title>{{ userName }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list-item v-for="(item, index) in items" :key="index" :to="item.link">
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        { title: '連絡先一覧', icon: 'list' }
+      ]
+    }
+  }
+}
+</script>
