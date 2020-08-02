47aa15a1bd924e0671c9525d6384194f57965f0d
kimiaki2510 <153046sk@gmail.com>
2020-08-02 15:00:16 +0900

    store

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,34 +1,40 @@
 <template>
   <v-layout wrap style="height: 200px;">
-  <v-navigation-drawer v-model="$store.state.drawer" absolute temporary>
-    <v-list>
-      <v-list-item>
-        <v-list-item-avatar>
-          <img v-if="photoURL" :src="photoURL">
-        </v-list-item-avatar>
-        <v-list-item-content>
-          <v-list-item-title>{{ userName }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+    <v-navigation-drawer v-model="$store.state.drawer" absolute temporary>
+      <v-list class="pa-1">
+        <v-list-tile avatar>
+          <v-list-tile-avatar>
+            <img src="https://avatars2.githubusercontent.com/u/1363954?s=460&v=4">
+          </v-list-tile-avatar>
 
-      <v-divider></v-divider>
+          <v-list-tile-content>
+            <v-list-tile-title>Kazuya Kojima</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
 
-      <v-list-item v-for="(item, index) in items" :key="index" :to="item.link">
-        <v-list-item-icon>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-item-icon>
-        <v-list-item-content>
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
-  </v-navigation-drawer>
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+
+        <v-list-tile v-for="item in items" :key="item.title" >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </v-layout>
 </template>
 
 <script>
 export default {
   data () {
     return {
+      
       items: [
         { title: '連絡先一覧', icon: 'list' }
       ]
